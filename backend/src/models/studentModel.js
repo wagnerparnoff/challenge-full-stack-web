397acd63ec7fb9460a0a7d8dfb023323355ab795
wagnerparnoff <wagnerparnoffpereira@gmail.com>
2024-03-08 15:18:58 -0300

    model.js

---
+++ backend/src/models/studentModel.js
@@ -0,0 +1,30 @@
+import { DataTypes } from "sequelize";
+import sequelize from "../config/sequelize.js";
+
+const Aluno = sequelize.define("aluno", {
+  id: {
+    type: DataTypes.INTEGER,
+    primaryKey: true,
+    autoIncrement: true,
+  },
+  name: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  email: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  ra: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    unique: true,
+  },
+  cpf: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    unique: true,
+  }
+});
+
+export default Aluno;
